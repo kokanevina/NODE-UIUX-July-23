15f5eda8b57bc871511e80c8fd96b057e7eb0fe8
kokaneva <kokane.vina@gmail.com>
2023-07-19 18:00:32 +0530

    commit

---
+++ Sample Node Project UIUX/CustomModules/MathModule.js
@@ -0,0 +1,8 @@
+
+
+let add=(num1,num2)=>num1+num2;
+
+let subtract=(num1,num2)=>num1-num2;
+
+module.exports.addFun=add;
+module.exports.subtractFun=subtract;  
---
+++ Sample Node Project UIUX/CustomModules/UseModuleDemo.js
@@ -0,0 +1,5 @@
+let math=require('./MathModule');
+console.log(math);
+
+math.addFun(34,67);
+math.subtractFun(45,23);
