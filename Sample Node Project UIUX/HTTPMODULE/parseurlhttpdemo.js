2e2fe4669555ffe238d6112981db0fe656639042
kokaneva <kokane.vina@gmail.com>
2023-07-21 17:42:30 +0530

    commit

--- Sample Node Project UIUX/HTTPMODULE/parseurlhttpdemo.js
+++ Sample Node Project UIUX/HTTPMODULE/parseurlhttpdemo.js
@@ -4,13 +4,13 @@ const url = require('url');
 let httpServer=http.createServer((request,response)=>{
    let reqaddress=request.url;
    let parsedq=url.parse(reqaddress,true);
-   response.setHeader('Content-Type', 'text/html');
+   /* response.setHeader('Content-Type', 'text/html'); */
+   response.writeHead(200, {
+    'Content-Type': 'text/html',
+    }); 
     response.write("<p>HostName:"+request.headers.host+"</p>"); 
     response.write("<p>PathName:"+parsedq.pathname+"</p>");
     let myQuery=parsedq.query; 
-   /*  response.writeHead(200, {
-    'Content-Type': 'text/html',
-    }); */
     response.write("<p>Company Name:"+myQuery.companyName+"</p>");
     response.write("<p>Branch :"+myQuery.branch+"</p>");
     response.write("<p>Est. year :"+myQuery.eyear+"</p>");
