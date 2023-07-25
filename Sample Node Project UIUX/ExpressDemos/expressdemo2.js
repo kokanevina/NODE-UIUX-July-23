1169449da3fd6c826b02b8559277d9927bc55d21
kokaneva <kokane.vina@gmail.com>
2023-07-25 17:23:02 +0530

    commit

--- Sample Node Project UIUX/ExpressDemos/expressdemo2.js
+++ Sample Node Project UIUX/ExpressDemos/expressdemo2.js
@@ -8,4 +8,16 @@ server.use(express.static('Resources'));
 server.get('/login',(request, response)=>{
     response.sendFile("D://Sessions/NODE-UIUX-July-23/Sample Node Project UIUX/Resources/LoginForm.html");
 })
+server.get('/processdata_getrequest', (request,response)=>{
+    // get request : data is in link in key value pair  username=what u type, password=wat u type
+
+    console.log(request.query.username);
+    console.log(request.query.password);
+    //response.send("Welcome "+request.query.username);
+    let user ={
+        user_name:request.query.username,
+        password:request.query.password
+    }
+    response.send(JSON.stringify(user));
+})
 // do same to serve images e.g. /roseflower
